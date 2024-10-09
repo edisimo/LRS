e62c97c1632f5c4588014b8f3ddd8c54d84fbc2b
Eduard Zelenay <xzelenay@stuba.sk>
2024-10-09 16:23:21 +0200

    deleted build and stuff

--- proj1/build/template_drone_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lrs-ubuntu/proj1/src/template_drone_control
-# Build directory: /home/lrs-ubuntu/proj1/build/template_drone_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
