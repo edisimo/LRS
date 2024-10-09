52e92db270c53190d7fba6ae35f48cf3f9f168f7
Eduard Zelenay <xzelenay@stuba.sk>
2024-10-09 16:12:22 +0200

    initial_commit

---
+++ proj1/build/template_drone_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lrs-ubuntu/proj1/src/template_drone_control
+# Build directory: /home/lrs-ubuntu/proj1/build/template_drone_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ proj1/src/template_drone_control/build/CMakeFiles/template_drone_control_node.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for template_drone_control_node.
+# This may be replaced when dependencies are built.
