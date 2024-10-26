3fe2b5c5d6c2c722b9a9315342c4bcbf4c073109
Eduard Zelenay <xzelenay@stuba.sk>
2024-10-27 00:27:10 +0200

    parser

--- proj1/src/drone_control/include/drone_control/path_finding.hpp
+++ proj1/src/drone_control/include/drone_control/path_finding.hpp
@@ -1,6 +1,11 @@
 #ifndef PATH_FINDING_HPP
 #define PATH_FINDING_HPP
 #include <rclcpp/rclcpp.hpp>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 
 class MapLoading
 {
@@ -8,10 +13,24 @@ public:
     MapLoading();
 };
 
+struct Point {
+    double x_;
+    double y_;
+    double z_;
+    std::string precision_;
+    std::string command_;
+    Point(double x, double y, double z, const std::string& precision, const std::string& command)
+        : x_(x), y_(y), z_(z), precision_(precision), command_(command) {}
+};
+
 class MissionParser
 {
 public:
     MissionParser();
+    void ParsePoints(const std::string& filename);
+    void DisplayPoints();
+private: 
+    std::vector<Point> points_;
 };
 
 class PathFinding : public rclcpp::Node
--- proj1/src/drone_control/src/path_finding.cpp
+++ proj1/src/drone_control/src/path_finding.cpp
@@ -3,6 +3,8 @@
 PathFinding::PathFinding() : Node("path_finding_node")
 {
     RCLCPP_INFO(this->get_logger(), "HELLO WORLD");
+    mission_.ParsePoints("/home/lrs-ubuntu/LRS-FEI/mission_1_all.csv");
+    mission_.DisplayPoints();
 }
 
 MapLoading::MapLoading()
@@ -11,4 +13,44 @@ MapLoading::MapLoading()
 
 MissionParser::MissionParser()
 {
+}
+
+void MissionParser::ParsePoints(const std::string& filename) {
+    std::ifstream file(filename);
+    std::string line;   
+    while (std::getline(file, line)) {
+        std::istringstream ss(line);
+        std::string token;
+        std::vector<std::string> tokens;
+
+        while (std::getline(ss, token, ',')) {
+            tokens.push_back(token);
+        }
+
+        if (tokens.size() == 5) {
+            double x = std::stod(tokens[0]);
+            double y = std::stod(tokens[1]);
+            double z = std::stod(tokens[2]);
+            std::string precision = tokens[3];
+            std::string command = tokens[4];
+            points_.emplace_back(x, y, z, precision, command);
+        } 
+        else {
+            // TODO: find out how to use a dummy logger
+            // rclcpp::Logger logger("");
+            // RCLCPP_ERROR(logger, "Failed to parse file");
+            std::cout << "Parsing failed" << std::endl;
+            return;
+        }
+    }
+    std::cout << "Finished" << std::endl;
+}
+
+void MissionParser::DisplayPoints() {
+    for (const auto& point : points_) {
+        std::cout << "Point: (" << point.x_<< ", " << point.y_ << ", " << point.z_
+                  << "), Precision: " << point.precision_
+                  << ", Command: " << (point.command_.empty() ? "None" : point.command_)
+                  << std::endl;
+    }
 }
